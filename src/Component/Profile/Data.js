afee1ab047cb26ecb6a5ffde5f09a52c3298a709
darshana Shingade <darshanahingade.gmail.com>
2023-02-28 01:25:36 +0530

    first commit

---
+++ src/Component/Profile/Data.js
@@ -0,0 +1,27 @@
+const Data =[
+    {
+        name : "Diance Cooper",
+        email:"diancecooper@gmail.com",
+        gender:"female",
+        birthdate:"03/07/1995",
+        phone_number:"8169730388",
+        member_status:"Active member"
+    },
+    {
+        name : "Diance Cooper",
+        email:"diancecooper@gmail.com",
+        gender:"female",
+        birthdate:"03/07/1995",
+        phone_number:"8169730388",
+        member_status:"Active member"
+    },
+    {
+        name : "Diance Cooper",
+        email:"diancecooper@gmail.com",
+        gender:"female",
+        birthdate:"03/07/1995",
+        phone_number:"8169730388",
+        member_status:"Active member"
+    },
+
+]
